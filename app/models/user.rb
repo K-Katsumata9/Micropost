946f076020092c5e929557994a42142dde461087
K-Katsumata9 <dowbler@icloud.com>
2024-03-10 22:13:09 +0900

    フォロー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,16 @@
     
     has_many :posts, dependent: :destroy
 
+    has_many :active_relationships, class_name:  "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed
+
+    has_many :passive_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+    has_many :followers, through: :passive_relationships, source: :follower
+  
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
@@ -78,10 +88,30 @@
       self.reset_sent_at < 10.minutes.ago
     end
 
-    def feed
-      Post.where("user_id = ?", id)
-    end
+    # ユーザーのステータスフィードを返す
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+             .includes(:user, image_attachment: :blob)
+  end
 
+    # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id) unless self == other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーが他のユーザーをフォローしていればtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
     private
 
       # メールアドレスをすべて小文字にする
